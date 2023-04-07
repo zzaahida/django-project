a6cb5a93c3c8204b03f64b7f21a031c9ed7607d6
Zahida <zahida.yermagambet@mail.ru>
2023-04-07 18:23:01 +0600

    lab 9

--- women/utils.py
+++ women/utils.py
@@ -4,12 +4,11 @@ from .models import *
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}
+        {'title': "Обратная связь", 'url_name': 'contact'}
 ]
 
 class DataMixin:
-    paginate_by = 2
+    paginate_by = 20
 
     def get_user_context(self, **kwargs):
         context = kwargs
